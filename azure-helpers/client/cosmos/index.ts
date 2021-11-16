0d1ee1900d9a13597f34fcc7fd68d7ba876efc6f
TJ <thaddeusjiang@gmail.com>
2021-11-16 19:07:03 +0900

    refact: DB_CONNECTION_STRING

--- azure-helpers/client/cosmos/index.ts
+++ azure-helpers/client/cosmos/index.ts
@@ -1,8 +1,8 @@
 import { CosmosClient } from "@azure/cosmos";
 
-const CONNECTION_STRING = process.env.CONNECTION_STRING;
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 
-const client = new CosmosClient(CONNECTION_STRING);
+const client = new CosmosClient(DB_CONNECTION_STRING);
 
 export const deleteCosmosContainers = async (
   databaseId: string,
